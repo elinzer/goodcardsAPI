07c0e289cbc3da5ca4bc1f062abb69bbe2e27fb8
el linzer <elinzer5@gmail.com>
2025-01-17 12:02:42 -0600

    stop working on main

--- goodcards2/cards/api/serializers.py
+++ goodcards2/cards/api/serializers.py
@@ -1,12 +1,9 @@
 from rest_framework import serializers
 from cards.models import Card
 
-class CardSerializer(serializers.Serializer):
-    name = serializers.CharField()
-    color_identity = serializers.CharField()
-    card_type = serializers.CharField()
-    rarity = serializers.CharField()
-    mtg_set = serializers.CharField()
-    text = serializers.CharField()
-    image_url = serializers.URLField()
+class CardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Card
+        fields = "__all__"
+
     
