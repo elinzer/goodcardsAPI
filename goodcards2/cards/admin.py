5057aa3ffd81f3b4ab0231f68abf4559edd24c9a
el linzer <elinzer5@gmail.com>
2025-01-20 14:22:45 -0600

    added more models

--- goodcards2/cards/admin.py
+++ goodcards2/cards/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from cards.models import Card
+from cards.models import Card, Deck, DeckCard
 
 # Register your models here.
 admin.site.register(Card)
+admin.site.register(Deck)
+admin.site.register(DeckCard)
--- goodcards2/cards/models.py
+++ goodcards2/cards/models.py
@@ -1,4 +1,8 @@
+from tkinter.constants import CASCADE
+
 from django.db import models
+from user.models import User
+
 
 # Create your models here.
 class Card(models.Model):
@@ -10,6 +14,18 @@ class Card(models.Model):
     text = models.TextField(max_length=2000)
     image_url = models.URLField(max_length=1000)
     
-    
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
+
+
+class Deck(models.Model):
+    name = models.CharField(max_length=120)
+    user = models.ForeignKey(User, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class DeckCard(models.Model):
+    deck = models.ForeignKey(Deck, on_delete=CASCADE)
+    card = models.ForeignKey(Card, on_delete=CASCADE)
